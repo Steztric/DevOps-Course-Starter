45282bb242ddd67b626b11fc2432e24f7c670c20
Sterren Fisher <sterren.fisher@hotmail.co.uk>
2020-05-31 16:43:42 +0100

    Added routes for showing to-do items and adding a new item

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from flask import Flask, render_template, request, redirect, url_for
 import session_items as session
+from forms import AddToDoForm
 
 app = Flask(__name__)
 app.config.from_object('flask_config.Config')
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('index.html', items=session.get_items())
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    form = AddToDoForm()
+    if form.validate_on_submit():
+        print('Adding To-Do Item: ' + form.description.data)
+        session.add_item(form.description.data)
+        return redirect('/')
+    return render_template('add.html', title='Add Item', form=form)
 
 if __name__ == '__main__':
     app.run()
